e80e8a055fe88824690d83cbb2e7a97a65d306b6
hasash <hasan.ashraf2@mail.dcu.ie>
2020-12-14 12:02:44 +0000

    Camera Activity Working Name Field Updated Address Field Required for Send Button Email Populated Spinner to Resolve as Condition

--- app/src/main/java/com/example/sdaassign32020/OrderTshirt.java
+++ app/src/main/java/com/example/sdaassign32020/OrderTshirt.java
@@ -9,10 +9,12 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.icu.text.CaseMap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.InputType;
@@ -22,6 +24,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,12 +36,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 
 /*
@@ -62,13 +74,23 @@ public class OrderTshirt extends Fragment {
     private static final int REQUEST_TAKE_PHOTO = 2;
     private static final String TAG = "OrderTshirt";
     public static final int CAMERA_PERM_CODE = 101;
-    private static final int Permission_Code = 1000;
-    private static final int Image_Capture_Code = 1001;
+    public static final int CAMERA_REQUEST_CODE = 102;
+    public static final int GALLERY_REQUEST_CODE = 105;
+
+    //private String currentPhotoPath;
+    private StorageReference storageReference;
 
     private static final int RESULT_OK = -1 ;
     Uri image_uri;
 
     private Context mContext;
+
+    private static String root1 = null;
+    private static String imageFolderPath = null;
+    private String imageName = null;
+    private static Uri fileUri = null;
+    private static final int CAMERA_IMAGE_REQUEST=1;
+
 
     //private String[] array = getResources().getStringArray(R.array.ui_time_entries);
 
@@ -90,16 +112,20 @@ public class OrderTshirt extends Fragment {
         meditDelivery.setImeOptions(EditorInfo.IME_ACTION_DONE);
         meditDelivery.setRawInputType(InputType.TYPE_CLASS_TEXT);
 
-        // Camera take image button
+
         mCameraImage = root.findViewById(R.id.imageView);
         mcaptureImage = root.findViewById(R.id.image_capture);
 
+
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         //set a listener on the the camera image
         mCameraImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(TAG, "Camera Image View Clicked");
                 askCameraPermissions();
+                //Log.d(TAG, "Camera image Clicked");
             }
         });
 
@@ -190,47 +216,76 @@ public class OrderTshirt extends Fragment {
                 //Permisions already granted
                 openCamera();
             }
-                    /*
-                    if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
-                            checkSelfPermission (Manifest.permission.WRITE_EXTERNAL_STORAGE)) == PackageManager.PERMISSION_DENIED){
-                            //permision not enabled
-                        String [] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                        // Show popup for requesting permission
-                        requestPermissions(permission, Permission_Code);
-                    }
-
-                     */
-
         }
         else {
-            //system <marsmallow
+            //system < marsmallow
             openCamera();
         }
-        //dispatchTakePictureIntent(v);
+        Log.d(TAG, "Camera Permission Granted");
+
     }
 
 
     public void openCamera() {
-        //ContentValues values = new ContentValues();
-        //values.put(MediaStore.Images.Media. TITLE, "New Picture");
-        //values.put(MediaStore.Images.Media. DESCRIPTION, "Captured Image");
-        //image_uri = getContentResolver
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+        Log.d(TAG, "Camera Activity Opened");
 
-        //image_uri = getContentResolver().insert(MediaStore, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 
-        //Camera intent
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
-        startActivityForResult(cameraIntent, Image_Capture_Code);
+        // Ensure that there's a camera activity to handle the intent
+        //if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            Log.d(TAG, "Camera if loop resolve");
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Log.d(TAG, "Camera if photo file null");
+                Uri photoURI = FileProvider.getUriForFile(getContext(),
+                        "dcu.sdaassign3.hasanashraf",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        //}
+
+
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private String getFileExt(Uri contentUri) {
+        ContentResolver c = getContext().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(c.getType(contentUri));
     }
 
     // handle permisison result. if permission granted or not
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "Camera onrequest Permision");
 
         switch (requestCode){
-            case Permission_Code: {
+            case CAMERA_PERM_CODE: {
                 if (grantResults.length > 0 && grantResults [0] == PackageManager.PERMISSION_GRANTED) {
                     //permision granted fomr pop up
                     openCamera();
@@ -243,8 +298,48 @@ public class OrderTshirt extends Fragment {
         }
     }
 
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        Log.d(TAG, "Camera OnActivityResult");
+
+        if(requestCode == CAMERA_REQUEST_CODE){
+            if(resultCode == Activity.RESULT_OK){
+                File f = new File(currentPhotoPath);
+                mCameraImage.setImageURI(Uri.fromFile(f));
+                Log.d("tag", "ABsolute Url of Image is " + Uri.fromFile(f));
+
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                Uri contentUri = Uri.fromFile(f);
+                mediaScanIntent.setData(contentUri);
+                //this.send
+                getContext().sendBroadcast(mediaScanIntent);
+
+                uploadImageToFirebase(f.getName(),contentUri);
+
+
+
+            }
+
+        }
+
+        if(requestCode == GALLERY_REQUEST_CODE){
+            if(resultCode == Activity.RESULT_OK){
+                Uri contentUri = data.getData();
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_" + timeStamp +"."+getFileExt(contentUri);
+                Log.d("tag", "onActivityResult: Gallery Image Uri:  " +  imageFileName);
+                mCameraImage.setImageURI(contentUri);
+
+                uploadImageToFirebase(imageFileName,contentUri);
+
+
+            }
+
+        }
+
+
+    }
 
 
 
@@ -269,7 +364,7 @@ public class OrderTshirt extends Fragment {
  */
 
 
-    }
+
     /*
     //Take a photo note the view is being passed so we can get context because it is a fragment.
     //update this to save the image so it can be sent via email
@@ -285,6 +380,32 @@ public class OrderTshirt extends Fragment {
     /*
      * Returns the Email Body Message, update this to handle either collection or delivery
      */
+
+    private void uploadImageToFirebase(String name, Uri contentUri) {
+        Log.d(TAG, "Camera Upload Image to Firebase");
+        final StorageReference image = storageReference.child("pictures/" + name);
+        image.putFile(contentUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                image.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Log.d("tag", "onSuccess: Uploaded Image URl is " + uri.toString());
+                    }
+                });
+
+                Toast.makeText(getContext(), "Image Is Uploaded.", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), "Upload Failled.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
     private String createOrderSummary(View v)
     {
         String orderMessage = "";
@@ -304,7 +425,10 @@ public class OrderTshirt extends Fragment {
     private void sendEmail(View v)
     {
         //check that Name is not empty, and ask do they want to continue
+
         String customerName = mCustomerName.getText().toString();
+        String recipientlist = "mytshirt@dcu.ie";
+
         if (mCustomerName == null || customerName.equals(""))
         {
             Toast.makeText(getContext(), "Please enter your name", Toast.LENGTH_SHORT).show();
@@ -315,6 +439,14 @@ public class OrderTshirt extends Fragment {
 
         } else {
             Log.d(TAG, "sendEmail: should be sending an email with "+createOrderSummary(v));
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_EMAIL, recipientlist);
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Order Request");
+            intent.putExtra(Intent.EXTRA_TEXT, createOrderSummary(v));
+
+            intent.setType("message/rfc822");
+            startActivity(Intent.createChooser(intent, "choose an email client"));
         }
     }
 
